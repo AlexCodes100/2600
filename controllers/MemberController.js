d60e3f3322d2b9e34c20ba7e69c4087cb5f5c9d2
Alex Mendez <rmendezmartine00@mylangara.ca>
2024-04-09 21:31:22 -0700

    Implemented local mongoDB for signin, signup and logout.

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -1,92 +1,67 @@
-const config = require(`${__dirname}/../server/config/config`)
-const utils = require(`${__dirname}/../server/utils`)
-const user = require(`${__dirname}/../models/user`)
-const express = require("express")
-const bcrypt = require("bcrypt")
-const memberController = express.Router()
-let members = []
-let authenticated = []
-memberController.post('/signup', async (request, response) => {
-    //let collection = client.db().collection('members')
-    //members = await util.find(collection, {})
-    ///console.log('MongoDB Members', members)
-    console.info(`\t|Inside app.post('/signup')`)
-    const { email, password } = request.body
-    console.log(`\t|Password = ${password}`)
-    let hashed = await bcrypt.hash(password, config.SALT_ROUNDS)
-    console.log(`${password} hash is ${hashed}`)
-    const member = user(email, hashed)
-    if (members.length === 0)
-        members = utils.readJson(config.MEMBERS)
-    //console.log(members)
-    const isMember = members.filter((m) => m.email === email)[0]
+const config = require(`${__dirname}/../server/config/config`);
+const utils = require(`${__dirname}/../server/utils`);
+const User = require(`${__dirname}/../models/user`); // Import the Mongoose User model
+const express = require("express");
+const bcrypt = require("bcrypt");
+const memberController = express.Router();
+
+memberController.post("/signup", async (request, response) => {
+    console.info(`\t|Inside app.post('/signup')`);
+    const { email, password } = request.body;
+    console.log(`\t|Password = ${password}`);
+    let hashed = await bcrypt.hash(password, config.SALT_ROUNDS);
+    console.log(`${password} hash is ${hashed}`);
+    const member = new User({ email: email, hashedPassword: hashed });
+
+    const isMember = await User.findOne({ email: email }); // Use Mongoose User model
     if (!isMember) {
-        members.push(member)
-        console.info(members)
-        authenticated.push(email)
-        //util.insertOne(collection, members[members.length - 1])
-        utils.saveJson(config.MEMBERS, JSON.stringify(members))
-        response
-            .status(200)
-            .json({
-                success: {
-                    email: email,
-                    message: `${email} was added successfuly to members.`,
-                },
-            })
+        await member.save(); // Use Mongoose save method
+        response.status(200).json({
+            success: {
+                email: email,
+                message: `${email} was added successfully to members.`,
+            },
+        });
     } else {
         response
             .status(200)
-            .json({ error: `${email} already exists. Choose a different email` })
+            .json({ error: `${email} already exists. Choose a different email` });
     }
-})
+});
 
-memberController.post('/signin', async (request, response) => {
-    //let collection = client.db().collection('members')
-    //members = await util.find(collection, {})
-    ///console.log('MongoDB Members', members)
-    console.info(`\t|Inside app.post('/signin')`)
-    const { email, password } = request.body
-    if (members.length === 0)
-        members = utils.readJson(config.MEMBERS)
-    console.log(members)
+memberController.post("/signin", async (request, response) => {
+    console.info(`\t|Inside app.post('/signin')`);
+    const { email, password } = request.body;
+
     const error = {
         email: email,
         error: `Email or password is incorrect.`,
-    }
-    const member = members.filter((m) => m.email === email)[0]
+    };
+    const member = await User.findOne({ email: email }); // Use Mongoose User model
 
     if (!member) {
-        response
-            .status(200)
-            .json(error)
+        response.status(200).json(error);
     } else {
         const isMatched = await bcrypt.compare(password, member.hashedPassword);
         if (!isMatched) {
-            response
-                .status(200)
-                .json(error)
+            response.status(200).json(error);
         } else {
             response
                 .status(200)
-                .json({ success: `${email} logged in successfully!` })
-            authenticated.push(email)
+                .json({ success: `${email} logged in successfully!` });
         }
     }
-})
-memberController.post('/signout', (request, response) => {
-    console.log('inside /signout')
-    email = request.body.email
-    console.log("authenticated", authenticated)
-    authenticated.splice(authenticated.indexOf(email), 1)
-    console.log("authenticated", authenticated)
-    response
-        .status(200)
-        .json({
-            success: {
-                email: email,
-                message: `${email} logout successfully.`,
-            },
-        })
-})
-module.exports = memberController
+});
+
+memberController.post("/signout", (request, response) => {
+    console.log("inside /signout");
+    email = request.body.email;
+    response.status(200).json({
+        success: {
+            email: email,
+            message: `${email} logout successfully.`,
+        },
+    });
+});
+
+module.exports = memberController;
